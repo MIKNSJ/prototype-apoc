12a233ca8ae75521f79f1c2cea2add8785550646
MIKNSJ <sculetoncode@gmail.com>
2024-04-18 21:46:16 -0700

    projectiles init

--- src/apoc.cpp
+++ src/apoc.cpp
@@ -8,6 +8,10 @@
 
 #include <SFML/Graphics.hpp>
 #include "Input.hpp"
+#include <vector>
+#include <iostream>
+#include <math.h>
+using namespace std;
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
@@ -37,7 +41,19 @@ int main()
     health_bar.setOutlineThickness(5);
     health_bar.setOrigin(-490, -650);
 
+    // test projectiles
+    sf::RectangleShape bullet(sf::Vector2f(15, 5));
+    bullet.setFillColor(sf::Color::Yellow);
+    vector<sf::RectangleShape> bullets;
+    //vector<sf::CircleShape> bullets;
+    sf::Clock clock; // starts the clock
+    vector<float> direction;
+    //sf::Vector2f bullet_pos(shape.getPosition().x + shape.getRadius(), shape.getPosition().y + shape.getRadius());
 
+    // crosshair
+    sf::CircleShape crosshair(5.f);
+    crosshair.setFillColor(sf::Color::Red);
+    
     // setup window options: open and close
     while (window.isOpen())
     {
@@ -51,9 +67,51 @@ int main()
         window.clear();
         window.draw(health_bar);
         window.draw(shape);
+        window.draw(crosshair);
+
+        crosshair.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+        sf::Time fire_rate = clock.getElapsedTime();
+        sf::Vector2f bullet_pos(shape.getPosition().x + shape.getRadius(), shape.getPosition().y + shape.getRadius());
+
+        //double player_pos_x = shape.getPosition().x;
+        //double player_pos_y = shape.getPosition().y;
+
+        //cout << shape.getPosition().x << endl;
+        //cout << shape.getPosition().y << endl;
+        //cout << clock.getElapsedTime().asSeconds() << endl;
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            if (fire_rate.asSeconds() >= 0.3f) {
+                fire_rate = clock.restart();
+                //bullet.setPosition(shape.getPosition());
+                bullet.setPosition(bullet_pos);
+                bullets.push_back(bullet);
+                direction.push_back(atan2(sf::Mouse::getPosition(window).y - shape.getPosition().y,
+                    sf::Mouse::getPosition(window).x - shape.getPosition().x));
+            }
+
+            /* bullets.push_back(sf::CircleShape());
+            bullets.back().setRadius(5);
+            bullets.back().setOrigin(5,5);
+            bullets.back().setPosition(shape.getPosition());
+            direction.push_back(atan2(sf::Mouse::getPosition(window).y - shape.getPosition().y,
+                    sf::Mouse::getPosition(window).x - shape.getPosition().x)); */
+        }
+
+        for (int i = 0; i < (int)bullets.size(); i++) {
+            window.draw(bullets[i]);
+            bullets[i].move(0.5f * cos(direction[i]), 0.5f * sin(direction[i]));
+
+            if (bullets[i].getPosition().x > 1000 || bullets[i].getPosition().x < 0 ||
+                bullets[i].getPosition().y < 0 || bullets[i].getPosition().y > 700) {
+                bullets.erase(bullets.begin() + i);
+                direction.erase(direction.begin() + i);
+            }
+        }
         window.display();
 
         input(shape);
+        // cout << shape.getPosition().x << " " << shape.getPosition().y << endl;
     }
 
     return 0;
