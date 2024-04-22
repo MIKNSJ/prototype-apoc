411a9ead030c77683359c37650186fd679173e6e
MIKNSJ <sculetoncode@gmail.com>
2024-04-21 22:08:33 -0700

    120fps adjustment and enemy spawn tweaked

--- src/Input.cpp
+++ src/Input.cpp
@@ -19,27 +19,27 @@ void input(sf::CircleShape& shape) {
     // move up
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
         sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        shape.move(0.f, -0.25f);
+        shape.move(0.f, -2.5f);
     }
 
     // move down
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ||
         sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
     // left key is pressed: move our character
-        shape.move(0.f, 0.25f);
+        shape.move(0.f, 2.5f);
     }
 
     // move left
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ||
         sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
     // left key is pressed: move our character
-        shape.move(-0.25f, 0.f);
+        shape.move(-2.5f, 0.f);
     }
 
     // move right
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ||
         sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
     // left key is pressed: move our character
-        shape.move(0.25f, 0.f);
+        shape.move(2.5f, 0.f);
     }
 }
